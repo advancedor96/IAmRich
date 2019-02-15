b89195a50dd2d15df54e1df4a8a5d107294b7052
丁丁 <advancedor96@gmail.com>
2019-02-15 23:52:11 +0800

    加入 social share

--- app/components/Home.vue
+++ app/components/Home.vue
@@ -3,7 +3,9 @@
     <StackLayout>
       <!-- <Image src="~/diamond.png" stretch="none" /> -->
       <Carousel>
-        <CarouselItem id="slide1" class="slide slide1"></CarouselItem>
+        <CarouselItem id="slide1" class="slide slide1">
+          <Button text="Share" @tap="share"></Button>
+        </CarouselItem>
         <CarouselItem id="slide2" class="slide slide2"></CarouselItem>
         <CarouselItem id="slide2" class="slide slide3"></CarouselItem>
         <CarouselItem id="slide2" class="slide slide4"></CarouselItem>
@@ -13,7 +15,15 @@
 </template>
 
 <script>
+var SocialShare = require("nativescript-social-share");
+var imageSourceModule = require("image-source");
 export default {
+  methods:{
+    share(){
+      var image = imageSourceModule.fromFile("~/diamond.png");
+      SocialShare.shareImage(image);
+    }
+  }
 };
 </script>
 
